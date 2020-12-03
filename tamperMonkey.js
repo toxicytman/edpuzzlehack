9a8b9f61fe4597aa01a71a0c6a4fd149b695e880
DylanBruner <61059252+DylanBruner@users.noreply.github.com>
2020-12-03 06:16:29 -0800

    Create tamperMonkey.js

---
+++ tamperMonkey.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Edpuzzle Cheats
+// @version      1
+// @description  Gets Ed Puzzle awnsers
+// @author       Teach Me Python
+// @match        *://edpuzzle.com/assignments/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
